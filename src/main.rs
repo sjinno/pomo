383fb1d972a36be570d7ce4ce3eaf6c1a1db16bb
Shohei Jinno <shohei.jinno@gmail.com>
2021-03-13 14:47:23 -0800

    Basic functionalities are done.

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use rodio::{self, OutputStreamHandle};
+use std::io;
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    let (_stream, stream_handle) = rodio::OutputStream::try_default().unwrap();
+    loop {
+        println!("How many minutes are you going to study?");
+        let mut duration = String::new();
+        io::stdin()
+            .read_line(&mut duration)
+            .expect("Failed to read line.");
+        let duration = match duration.trim().parse::<u64>() {
+            Ok(num) => num * 1000 * 60,
+            Err(_) => continue,
+        };
+        begin_block(&stream_handle);
+        thread::sleep(Duration::from_millis(duration));
+        break;
+    }
+    end_block(&stream_handle);
+}
+
+fn begin_block(stream_handle: &OutputStreamHandle) {
+    thread::sleep(Duration::from_millis(500));
+    println!("New block has begun.");
+    let file = std::fs::File::open("audio/mario.wav").unwrap();
+    let mario = stream_handle.play_once(io::BufReader::new(file)).unwrap();
+    mario.set_volume(0.3);
+    thread::sleep(Duration::from_millis(1500));
+}
+
+fn end_block(stream_handle: &OutputStreamHandle) {
+    println!("Block has ended.");
+    let file = std::fs::File::open("audio/chime.ogg").unwrap();
+    let chime = stream_handle.play_once(io::BufReader::new(file)).unwrap();
+    chime.set_volume(0.3);
+    chime.sleep_until_end();
+}
