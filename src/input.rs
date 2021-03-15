6a3cda2624a6f8214ecf2866d7603079f5f39a81
Shohei Jinno <shohei.jinno@gmail.com>
2021-03-14 22:10:33 -0700

    Update.

--- src/input.rs
+++ src/input.rs
@@ -1,11 +1,20 @@
 use std::io;
 
-pub fn get_inputs() -> (u64, String) {
-    (get_mins(), get_title())
+pub fn get_inputs() -> (String, u64) {
+    (get_title(), get_mins())
+}
+
+fn get_title() -> String {
+    println!("What is the name of the task? (default: Block)");
+    let mut title = String::new();
+    io::stdin()
+        .read_line(&mut title)
+        .expect("Failed to read line.");
+    title.trim().to_string()
 }
 
 fn get_mins() -> u64 {
-    println!("How many minutes are you going to study?");
+    println!("How many minutes is it going to take?");
     let mut duration = String::new();
     io::stdin()
         .read_line(&mut duration)
@@ -15,13 +24,4 @@ fn get_mins() -> u64 {
         Err(_) => get_mins(),
     };
     duration
-}
-
-fn get_title() -> String {
-    println!("What would you like to name your task? (default: Block)");
-    let mut title = String::new();
-    io::stdin()
-        .read_line(&mut title)
-        .expect("Failed to read line.");
-    title.trim().to_string()
 }
--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ pub fn run() {
 }
 
 fn do_the_thing() {
-    let (mins, title) = input::get_inputs();
+    let (title, mins) = input::get_inputs();
     let (_stream, stream_handle) = rodio::OutputStream::try_default().unwrap();
     message::begin_block(&stream_handle, &title);
     progress::update_progress(&mins);
