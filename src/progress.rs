79c5b3617c45f796ee2f64e435f733f147eefb99
Shohei Jinno <shohei.jinno@gmail.com>
2021-03-16 15:37:30 -0700

    Add feature.
    1. Create color! macro to color given string values.
    2. Every 10 secs -> ● with default color.
    3. Every 1 min   -> ● with green color.

--- src/progress.rs
+++ src/progress.rs
@@ -1,24 +1,50 @@
 use std::io::Write;
 use std::thread;
 use std::time::Duration;
+use termion::clear;
+use termion::color::Green;
+
+// Formats string with color.
+// Requires `termion` crate.
+#[macro_export]
+macro_rules! color {
+    ($clr:expr, $val:expr) => {{
+        use termion::color::{Fg, Reset};
+        format!("{c1}{}{c0}", $val, c1 = Fg($clr), c0 = Fg(Reset))
+    }};
+
+    ($clr:expr, $val:expr, $opt:expr) => {{
+        use termion::color::{Fg, Reset};
+        format!("{c1}{}{c0}{}", $val, $opt, c1 = Fg($clr), c0 = Fg(Reset))
+    }};
+}
 
 pub fn update_progress(duration: &u64) {
-    let mut count = 0;
+    let mut count = 1;
     let mut progress = String::new();
-
+    // Duration is in secs.
+    // e.g. 1 min -> 60 secs
     while &count != duration {
-        if count % 60 == 0 && count != 0 {
-            progress.push_str("● ");
+        if count != 0 {
+            match count {
+                five_mins if five_mins % 300 == 0 => {
+                    let color = color!(Green, "●", " | ");
+                    progress = progress.replace("● ● ● ● ● ", &color);
+                }
+                one_min if one_min % 60 == 0 => {
+                    let color = color!(Green, "●", " ");
+                    progress = progress.replace("● ● ● ● ● ", &color);
+                }
+                ten_secs if ten_secs % 10 == 0 => progress.push_str("● "),
+                _ => (),
+            }
         }
-        if count % 300 == 0 && count != 0 {
-            progress.push_str("| ");
-        }
-        print!("\r{}", progress);
+        print!("\r{clr}{}", progress, clr = clear::CurrentLine,);
         std::io::stdout().flush().unwrap();
-
         thread::sleep(Duration::from_secs(1));
         count += 1;
     }
-    progress.push('●');
-    println!("\r{}", progress);
+    let color = color!(Green, "●");
+    progress = progress.replace("● ● ● ● ● ", &color);
+    println!("\r{clr}{}", progress, clr = clear::CurrentLine);
 }
